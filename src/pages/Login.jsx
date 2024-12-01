11def222ed9778fb0549764329a488e6f8189398
Nandani <nks854338@gmail.com>
2024-12-01 18:49:40 +0530

    commited

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -15,6 +15,7 @@ const Login = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
+
       const response = await axios.post(
         "https://dreamify-backend.vercel.app/users/login", 
         { email, password },
